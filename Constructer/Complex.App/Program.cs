06508d7d3ed5d32283b115c7e9a8e73e84db518a
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-08-01 15:43:58 +0300

    Add files via upload

---
+++ Constructer/Complex.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using ComplexNum;
+using System;
+
+namespace Complex.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ComplexNumber c1 = new ComplexNumber(3, 5);
+            ComplexNumber c2 = new ComplexNumber(1, 5);
+
+            ComplexNumber.Add(c1, c2);
+
+            //---Random Complex---//
+            ComplexNumber c3 = new ComplexNumber();
+            ComplexNumber c4 = new ComplexNumber();
+            ComplexNumber.Add(c3, c4);
+
+
+
+        }
+    }
+}
---
+++ Constructer/ComplexNum/ComplexNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ComplexNum
+{
+    public class ComplexNumber
+    {
+        public int Reel { get; }
+        public int Imajinel { get; }
+
+        public ComplexNumber()
+        {
+            Random rnd = new Random();
+            Reel = rnd.Next(1, 9);
+            Imajinel = rnd.Next(1, 9);
+
+        }
+
+        public ComplexNumber(int reel, int imajinel)
+        {
+            Reel = reel;
+            Imajinel = imajinel;
+        }
+
+        public static String Add(ComplexNumber c1, ComplexNumber c2)
+        {
+            Console.WriteLine($"{c1.Reel + c2.Reel}i + {c2.Imajinel + c2.Imajinel}j");
+            return $"{c1.Reel+c2.Reel}i + {c2.Imajinel+c2.Imajinel}j";
+        }
+
+
+    }
+}
