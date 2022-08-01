267f48deb59f889e1c724e1b3e7bb6b990d6a30a
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-08-01 15:24:21 +0300

    Add files via upload

---
+++ Constructer/ComplexClass/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ComplexClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+        }
+    }
+}
---
+++ Constructer/ComplexNum/ComplexNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ComplexNum
+{
+    public class ComplexNumber
+    {
+        public int Reel { get; }
+        public int Imajinel { get; }
+
+        public ComplexNumber(int reel, int imajinel)
+        {
+            Reel = reel;
+            Imajinel = imajinel;
+        }
+
+        public static String Add(ComplexNumber c1, ComplexNumber c2)
+        {
+            Console.WriteLine($"{c1.Reel + c2.Reel}i + {c2.Imajinel + c2.Imajinel}j");
+            return $"{c1.Reel+c2.Reel}i + {c2.Imajinel+c2.Imajinel}j";
+        }
+
+
+    }
+}
