4c88b0b28f55c0e85457e821061c2e6366feb03c
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-08-05 16:56:47 +0300

    Add files via upload

---
+++ Array_List1/ArrayList_1/Program.cs
@@ -0,0 +1,98 @@
+﻿
+using CreateArrayList;
+using System;
+
+namespace ArrayList_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyArrayList myArrayList = new MyArrayList();
+            myArrayList.Add("test1");
+            myArrayList.Add("test2");
+            myArrayList.Add("test3");
+            myArrayList.Add("test4");
+            myArrayList.Add("test5");
+            myArrayList.Add("test6");
+
+            Console.WriteLine("----------------Create Default ArrayList--------------");
+
+            for (int i = 0; i < myArrayList.Length; i++)
+            {
+                Console.WriteLine($"index[{i}]:{myArrayList[i]}");
+            }
+
+            Console.WriteLine("Length:"+myArrayList.Length);
+            Console.WriteLine("Uzunluk:" + myArrayList.UnNullOfNumnerElemant);
+
+
+
+
+            Console.WriteLine("----------------After Delte Comp.--------------");
+
+            Console.WriteLine("Delete test5");
+            myArrayList.DeleteEleman("test5");
+
+            Console.WriteLine("Delete test3");
+            myArrayList.DeleteEleman("test3");
+
+            for (int i = 0; i < myArrayList.Length; i++)
+            {
+                Console.WriteLine($"index[{i}]:{myArrayList[i]}");
+            }
+
+            Console.WriteLine("test1 is index of:" + myArrayList.IndexOf("test1"));
+            Console.WriteLine("test2 is index of:" + myArrayList.IndexOf("test2"));
+            Console.WriteLine("test3 is index of:" + myArrayList.IndexOf("test3"));
+            Console.WriteLine("test4 is index of:" + myArrayList.IndexOf("test4"));
+            Console.WriteLine("test5 is index of:" + myArrayList.IndexOf("test5"));
+            Console.WriteLine("test6 is index of:" + myArrayList.IndexOf("test6"));
+
+            Console.WriteLine("Length" + myArrayList.Length);
+            Console.WriteLine("Uzunluk" + myArrayList.UnNullOfNumnerElemant);
+
+            Console.WriteLine("----------------After Contains Comp.--------------");
+
+            Console.WriteLine("test1 have got : "+(myArrayList.Contains("test1") ? "have" : "have not"));
+            Console.WriteLine("test3 have got : " + (myArrayList.Contains("test3") ? "have" : "have not"));
+            Console.WriteLine("test2 is Contains: "+myArrayList.Contains("test2"));
+
+
+            Console.WriteLine("----------------After ArrayList[0] Comp.--------------");
+            Console.WriteLine("myArrayList[0]: "+myArrayList[0]);
+
+
+            Console.WriteLine("----------------After Crop Comp.--------------");
+            myArrayList.Crop();
+            for (int i = 0; i < myArrayList.Length; i++)
+            {
+                Console.WriteLine($"index[{i}]:{myArrayList[i]}");
+            }
+            Console.WriteLine("Length: " + myArrayList.Length);
+            Console.WriteLine("Uzunluk: " + myArrayList.UnNullOfNumnerElemant);
+
+
+
+            Console.WriteLine("----------------After Reverse Comp.--------------");
+            myArrayList.Reverse();
+            Console.WriteLine("Length: " + myArrayList.Length);
+            Console.WriteLine("Uzunluk: " + myArrayList.UnNullOfNumnerElemant);
+            for (int i = 0; i < myArrayList.Length; i++)
+            {
+                Console.WriteLine($"index[{i}]:{myArrayList[i]}");
+            }
+
+            Console.WriteLine("----------------After Again Reverse Comp.--------------");
+            myArrayList.Reverse();
+            Console.WriteLine("Length: " + myArrayList.Length);
+            Console.WriteLine("Uzunluk: " + myArrayList.UnNullOfNumnerElemant);
+            for (int i = 0; i < myArrayList.Length; i++)
+            {
+                Console.WriteLine($"index[{i}]:{myArrayList[i]}");
+            }
+
+        }
+
+    }
+}
---
+++ Array_List1/MyArrayList/MyArrayList.cs
@@ -0,0 +1,147 @@
+﻿using System;
+
+namespace CreateArrayList
+{
+    public class MyArrayList
+    {
+        private object[] myArrayList;
+        private int index = 0;
+
+        public int Length 
+        {
+            get { return myArrayList.Length; }
+        }
+
+        public int UnNullOfNumnerElemant //Uzunluk
+        {
+            get
+            {
+                var u = 0;
+                for (int i = 0; i <= myArrayList.Length-1; i++)
+                {
+                    if (myArrayList[i] != null)
+                    {
+                        u++;
+                    }
+                }
+
+                return u;
+            }
+        }
+
+        public object this[int index] 
+        {
+            get { return myArrayList[index]; }
+        }
+        public MyArrayList()
+        {
+            myArrayList = new object[5];
+        }
+
+        public MyArrayList(int arrayLength)
+        {
+            myArrayList = new object[arrayLength-1];
+        }
+
+
+        public void Add(object eleman)
+        {
+            CheckLengthOfArray();
+            myArrayList[index] = eleman;
+            ++index;
+        }
+
+
+        private void CheckLengthOfArray()
+        {
+            if (index == (myArrayList.Length -1))
+            {
+                Array.Resize(ref myArrayList, (myArrayList.Length * 2));
+            }
+        }
+
+        public bool Contains(object eleman)
+        {
+            bool flag = false;
+            for (int i = 0; i < myArrayList.Length; i++)
+            {
+                if (eleman == myArrayList[i])
+                {
+                    flag = true;
+                    break;
+                }
+                else
+                {
+                    flag = false;
+                }
+            }
+            return flag;
+        }
+
+        public int IndexOf(object eleman)
+        {
+            int index = -1;
+            for (int i = 0; i < myArrayList.Length; i++)
+            {
+                if (myArrayList[i] == eleman)
+                {
+                    index = i;
+                    break;
+                }
+            }
+            return index;
+        }
+
+        public void DeleteEleman(object eleman)
+        {
+            int index = IndexOf(eleman);
+            object[] tmp;
+
+            if (index == -1)
+            {
+                return;
+            }
+            else
+            {
+                tmp = new object[myArrayList.Length - 1];
+
+                for (int i = 0; i < index; ++i)
+                {
+                    tmp[i] = myArrayList[i];
+                }
+
+                for (int i = index; i < myArrayList.Length -1; ++i)
+                {
+                    tmp[i] = myArrayList[i+1];
+                }
+            }
+
+            myArrayList = tmp;
+        }
+
+        public void Crop()
+        {
+            object[] tmp = new object[this.UnNullOfNumnerElemant];
+            for (int i = 0; i <= this.UnNullOfNumnerElemant -1; ++i)
+            {
+                tmp[i] = myArrayList[i];
+            }
+            Array.Resize(ref myArrayList, this.UnNullOfNumnerElemant);
+            myArrayList = tmp;
+            
+        }
+
+        public void Reverse()
+        {
+            int index = 0;
+            object[] tmp = new object[this.UnNullOfNumnerElemant];
+            for (int i = this.UnNullOfNumnerElemant-1; i >= 0; --i)
+            {
+                tmp[index] = myArrayList[i];
+                index++;
+            }
+            myArrayList = tmp;
+
+        }
+    }
+}
