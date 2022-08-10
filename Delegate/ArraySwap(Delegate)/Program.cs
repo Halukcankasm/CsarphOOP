ff5c6ae0f6fd27500d54210295311cd9e2affbc8
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-08-10 16:11:09 +0300

    Add files via upload

---
+++ Delegate/ArraySwap(Delegate)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ArraySwap_Delegate_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int[] arr = new int[] { 6, 2, 32, 4, 51, 12 };
+
+            Shorting shorting = new Shorting();
+
+            shorting.Short(arr, new DelegateSwap(shorting.Swap));
+
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Delegate/ArraySwap(Delegate)/Shorting.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArraySwap_Delegate_
+{
+    public delegate void DelegateSwap(ref int[] arr, int x, int y);
+    public class Shorting
+    {
+        public void Swap(ref int[] arr, int x, int y)
+        {
+            var a = arr[x];
+            arr[x] = arr[y];
+            arr[y] = a;
+
+        }
+
+        public void Short(int[] arr, DelegateSwap del)
+        {
+            int t = 0;
+            for (int i = 0; i < arr.Length ; ++i)
+            {
+                t = i;
+                for (int index = i+1; index < arr.Length; ++index)
+                {
+                    if (arr[index] < arr[t])
+                    {
+                        t = index;
+                    }
+                }
+                del.Invoke(ref arr, i, t);
+            }
+        }
+    }
+}
