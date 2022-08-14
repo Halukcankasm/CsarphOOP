6dd43ad673d57fb6ca5f7c6e787ceee768042578
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-08-14 19:47:45 +0300

    Add files via upload

---
+++ SingletonPatern/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace SingletonPatern
+{
+    public class Sample
+    {
+
+        //Durum bilgisi(State)
+        private int m_data;
+        private static Sample m_tmp = null;
+
+        public int Data { get { return m_data; } }
+        private Sample(int prm)
+        {
+            Console.WriteLine("Sample const çağrıldı");
+            m_data = prm;
+        }
+
+        public static Sample CreateSample(int data)
+        {
+            if (m_tmp == null)
+            {
+                m_tmp = new Sample(prm: data);
+            }
+
+            return m_tmp;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sample sample =  Sample.CreateSample(1);
+            Sample sample2 = Sample.CreateSample(2);
+
+            Console.WriteLine("sample1:"+sample.Data);
+            Console.WriteLine("sample2:"+sample2.Data);
+        }
+    }
+}
