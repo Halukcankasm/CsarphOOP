ddd22a9cb36ae65cc6c74613a2325c8a3f24aed0
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-08-15 11:51:53 +0300

    Add files via upload

---
+++ Sigorta(Event)/SigortaKutusu/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SigortaKutusu
+{
+    public delegate void Del(int deger);
+    public class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+            Sigorta sigorta = new Sigorta();
+
+            Del del = new Del(sigorta.AkımKontrol);
+
+            for (int i = 0; i < 40; i++)
+            {
+                del.Invoke(i);
+            }
+
+            
+        }
+    }
+}
---
+++ Sigorta(Event)/SigortaKutusu/Sigorta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SigortaKutusu
+{
+    public class Sigorta
+    {
+        #region Veri Elemanları/Property
+        //State(Durum bilgisi) Özellikleri
+        private int anmaAkimi;
+
+        private int AnmaAkımı
+        {
+            get
+            {
+                return anmaAkimi;
+            }
+            set
+            {
+                anmaAkimi = value;
+            }
+        }
+
+        public int Akim
+        {
+            get
+            {
+                return 20;
+            }
+        }
+        #endregion
+
+        public void AkımKontrol(int _anmaAkimi)
+        {
+            if (_anmaAkimi > Akim)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Beep();
+                Console.WriteLine("Sigorta Attı: "+ _anmaAkimi);
+                Console.ResetColor();
+
+            }
+            else
+            {
+                Console.WriteLine("Anlık akımı değeri standart: "+ _anmaAkimi);
+            }
+        }
+    }
+}
