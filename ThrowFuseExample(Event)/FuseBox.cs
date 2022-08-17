f63fca6c1a531dfc8d4f155b2b47bf507e92db04
Halukcankasm <65433322+Halukcankasm@users.noreply.github.com>
2022-08-17 10:42:40 +0300

    Add files via upload

---
+++ ThrowFuseExample(Event)/FuseBox.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThrowFuseExample_Event_
+{
+    public delegate void DlgFuse(int instanceCurrent);
+    public class FuseBox
+    {
+        private event DlgFuse ThrowFuseEvent;
+        private string Type { get; set; }
+        private int FuseCurrent { get; set; }
+
+        public FuseBox(string _type, int _fuseCurrent)
+        {
+            Type = _type;
+            FuseCurrent = _fuseCurrent;
+        }
+
+        public void CheckCureent(int _instanceCurrent)
+        {
+            ThrowFuseEvent += new DlgFuse(Throw_Fuse);
+
+            if (_instanceCurrent > FuseCurrent)
+            {
+                if (ThrowFuseEvent != null)
+                {
+                    ThrowFuseEvent.Invoke(_instanceCurrent);
+                    //ThrowFuseEvent();
+                } 
+            }
+            else
+            {
+                Console.WriteLine($"{Type}, {_instanceCurrent}");
+            }
+        }
+
+        private void Throw_Fuse(int instanceCurrent)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($"Throw Fuse {instanceCurrent}, {Type}");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ ThrowFuseExample(Event)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ThrowFuseExample_Event_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FuseBox fuseBox = new FuseBox("siemens", 10);
+            for (int i = 0; i < 20; i++)
+            {
+                fuseBox.CheckCureent(i);
+            }
+        }
+    }
+}
